aa03d284e3ef3587dce2e5672c77e92b8d46bea2
Anawin A. <138203645+AnawinA@users.noreply.github.com>
2024-09-30 21:25:40 +0700

    separate css from html and adjust pywebview

--- main view.py
+++ main view.py
@@ -1,16 +1,16 @@
 """main view"""
 import webview
 from bs4 import BeautifulSoup
+import time
 
+def load_website(window: webview.Window):
+    """load website"""
+    pass
 
 def main():
     """pywebview"""
-    f = open('./it-encode-web.html', 'r')
-    fsimp = BeautifulSoup(f.read(), "html.parser").prettify()
-    f.close()
-
-    window = webview.create_window('Load HTML Example', html=fsimp)
-    webview.start()
+    window = webview.create_window('Load HTML Example', "./it-encode-web.html")
+    webview.start(load_website, window)
 
 if __name__ == '__main__':
     main()
