153ed0f20b125a7c1a1aeac8124f0c470993257d
Anawin A. <138203645+AnawinA@users.noreply.github.com>
2024-10-09 00:24:43 +0700

    add true n false encoding

---
+++ it_encode_lib/true_false/is_prime.py
@@ -0,0 +1,17 @@
+"""is prime"""
+
+def is_prime(n):
+    """is prime"""
+    if n <= 1:
+        return False
+    if n == 2:
+        return True
+    if not n % 2:
+        return False
+    limit = int(n**0.5) + 1
+    for i in range(3, limit, 2):
+        if not n % i:
+            return False
+    return True
+
+is_prime()
