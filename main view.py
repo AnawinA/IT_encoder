ba1ea8da0a2bd47a2f4aee5564897f6253b62601
Anawin A. <138203645+AnawinA@users.noreply.github.com>
2024-10-20 23:43:04 +0700

    fix for valid webview use

--- main view.py
+++ main view.py
@@ -3,14 +3,10 @@ import webview
 from bs4 import BeautifulSoup
 import time
 
-def load_website(window: webview.Window):
-    """load website"""
-    pass
-
 def main():
     """pywebview"""
-    window = webview.create_window('Load HTML Example', "./it-encode-web.html")
-    webview.start(load_website, window)
+    window = webview.create_window('Load HTML Example', "./index.html")
+    webview.start()
 
 if __name__ == '__main__':
     main()
