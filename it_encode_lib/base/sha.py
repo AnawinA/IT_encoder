92df85ce3682ddbfda281170cf1b1eb8528c80f5
K4TADusty <67070303@kmitl.ac.th>
2024-10-21 11:53:33 +0700

    Add files via upload

--- it_encode_lib/base/sha.py
+++ it_encode_lib/base/sha.py
@@ -10,3 +10,14 @@ def generate_sha1(input_string):
     
     return sha1_hash.hexdigest()
 
+"""Sha-2"""
+import hashlib
+def generate_sha256(input_string):
+    """Sha-2 hashing algorithm"""
+    encoded_string = input_string.encode('utf-8')
+
+    sha256_hash = hashlib.sha256()
+
+    sha256_hash.update(encoded_string)
+    
+    return sha256_hash.hexdigest()
