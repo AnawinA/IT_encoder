e88da5f0dc1eaf2460ad2cc0781d32eb8aa5b178
K4TADusty <67070303@kmitl.ac.th>
2024-10-21 12:00:57 +0700

    sha.py

--- it_encode_lib/base/sha.py
+++ it_encode_lib/base/sha.py
@@ -14,10 +14,10 @@ input_string = "Hello, World!"
 hash_result = generate_sha1(input_string)
 print(f"SHA-1 hash of '{input_string}': {hash_result}")
 
-"""Sha-2"""
+"""Sha-2 Family"""
 import hashlib
 def generate_sha256(input_string):
-    """Sha-2 hashing algorithm"""
+    """Sha-256 hashing algorithm"""
     encoded_string = input_string.encode('utf-8')
 
     sha256_hash = hashlib.sha256()
@@ -30,7 +30,21 @@ input_string = "Hello, World!"
 hash_result = generate_sha256(input_string)
 print(f"SHA-256 hash of '{input_string}': {hash_result}")
 
-"""Sha-512"""
+import hashlib
+def generate_sha384(input_string):
+    """Sha-384 hashing algorithm"""
+    encoded_string = input_string.encode('utf-8')
+
+    sha384_hash = hashlib.sha384()
+    
+    sha384_hash.update(encoded_string)
+    
+    return sha384_hash.hexdigest()
+
+input_string = "Hello, World!"
+hash_result = generate_sha384(input_string)
+print(f"SHA-384 hash of '{input_string}': {hash_result}")
+
 import hashlib
 def generate_sha512(input_string):
     """Sha-512 hashing algorithm"""
