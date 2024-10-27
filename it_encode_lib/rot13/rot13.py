62c5fcc469d1bdd065f167968d058d106eb9187c
Wasawat67070162 <67070162@kmitl.ac.th>
2024-10-28 01:46:42 +0700

    rot13 encoder

---
+++ it_encode_lib/rot13/rot13.py
@@ -0,0 +1,8 @@
+"""rot12Encode"""
+def rot13_encode(text):
+    """text to rot13"""
+    rot13 = str.maketrans(
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",
+        "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm"
+    )
+    return text.translate(rot13)
