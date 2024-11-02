35453b507c3e180ea51affea608a30d69a8d7804
apiphon <74528989+apiphon@users.noreply.github.com>
2024-11-02 21:13:29 +0700

    add temp unit

--- it_encode_lib/itf/network.py
+++ it_encode_lib/itf/network.py
@@ -1,7 +1,6 @@
 '''input ip address and subnet mask output network id'''
 def networkid(address,subnet):
     '''input address [255, 082, 255, 000] subnet [252, 127, 063, 006] list[4][3] '''
-    
     netid = []
     for i in range(4):
         atemp = ""
@@ -18,4 +17,3 @@ def networkid(address,subnet):
             astemp += str(i)
         netid.append(int(astemp,2))
     return netid
-print(networkid([161,246,38,35],[255,255,0,128]))
