2e0055dcb19f8c98d3dad163e3f1304a07857481
apiphon <74528989+apiphon@users.noreply.github.com>
2024-11-02 22:03:15 +0700

    add temp unit

---
+++ it_encode_lib/temperture/temp_change.py
@@ -0,0 +1,13 @@
+'''change unit of temp'''
+def c_to_f(temp):
+    '''cel to fah'''
+    return (temp*(9/5))+32
+def f_to_c(temp):
+    '''fah to cel'''
+    return (temp-32*(5/9))
+def c_to_k(temp):
+    '''cel to kavin'''
+    return (temp+273.15)
+def f_to_k(temp):
+    '''cel to kavin'''
+    return (temp*(9/5))+32+237.15
